b959801e267077cd4b103919571707e17c6c4bf4
paulhectork <paul.kervegan@hotmail.fr>
2025-11-14 18:35:31 +0100

    [SETUP] optional launch of aiiinotat

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,21 +1,32 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd );
+# build and serve the mirador-mae-aiiinotate ingration.
+# 2 modes are allowed: prod and dev, defined by $2.
+# if $2 == "dev", then we will run `aiiinotate` in a background process and exit it when quitting the app
+# don't use "dev" in prod.
 
-source "$SCRIPT_DIR/utils.sh"
+# allowed values; "dev" or nothing.
+MODE="$1"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd );
 
 cd "$ROOT_DIR"
 
+source "$SCRIPT_DIR/utils.sh"
 check_envfile;
-
 source "$ENV_FILE";
 
-aiiinotate --env "$ENV_FILE" -- serve prod &
+# start aiiinotate in dev
+if [ "$MODE" = "dev" ];
+then aiiinotate --env "$ENV_FILE" -- serve prod &
+fi;
 
 # build the mirador app
 rm -rf "$ROOT_DIR"/dist "$ROOT_DIR"/.parcel-cache && parcel build "$ROOT_DIR"/src/index.html;
 # serve it
 http-server "$ROOT_DIR"/dist -p "$MIRADOR_PORT";
 
-# once the above command returns, kill aiiinotate (MainThread = nodejs parent thread).
-kill $(pgrep MainThread);
+# in dev, kill aiiinotate when http-server exists (MainThread = nodejs parent thread).
+if [ "$MODE" = "dev" ];
+then kill $(pgrep MainThread);
+fi;
